9d63b6c8c5b3f952e245a2537e4e3915f8144702
Felipe Fernandes <felipedfe@gmail.com>
2022-01-29 13:05:45 -0300

    exercicios

---
+++ 01_fundamentos_WEB/bloco_06_forms_flexbox_e_responsivo/dia_02_bibliotecas_e_frameworks/script.js
@@ -0,0 +1,12 @@
+document.getElementById('input-data').DatePickerX.init();
+
+let data = document.getElementById("input-data").DatePickerX;
+
+console.log(data);
+
+console.log(document.getElementById("input-data"))
+
+console.log(data.getMaxDate());
+
+
+
