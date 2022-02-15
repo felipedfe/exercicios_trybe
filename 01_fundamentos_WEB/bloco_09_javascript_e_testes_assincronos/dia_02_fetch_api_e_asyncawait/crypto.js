d141e4298e8763a680d78fac5ba3df98c31ea379
Felipe Fernandes <felipedfe@gmail.com>
2022-02-15 10:45:38 -0300

    alteracao pokemon

--- 01_fundamentos_WEB/bloco_09_javascript_e_testes_assincronos/dia_02_fetch_api_e_asyncawait/crypto.js
+++ 01_fundamentos_WEB/bloco_09_javascript_e_testes_assincronos/dia_02_fetch_api_e_asyncawait/crypto.js
@@ -1,21 +1,54 @@
-let listaDeMoedas = document.querySelector('ul'); 
+////////////////////////////////// 1a resolução /////////////////////////////////////////
+
+// let listaDeMoedas = document.querySelector('ul'); 
+
+// const url = 'https://api.coincap.io/v2/assets';
+
+// function fetchCrypto() {
+//   fetch(url)
+//   .then((resposta) => resposta.json())
+//   .then((moedas) => {
+//     let arrayDasMoedas = moedas.data;
+//     let dezPrimeirasMoedas = arrayDasMoedas.slice(0, 10);
+
+//     dezPrimeirasMoedas.forEach((moeda) =>{
+//       let li = document.createElement('li')
+//       li.innerHTML = `${moeda.name} ${moeda.symbol} : ${moeda.priceUsd}`
+//       listaDeMoedas.appendChild(li)
+//     })
+//   })
+//   .catch((erro) => console.log(`Deu ruim! ${erro}`))
+// }
+
+// fetchCrypto();
+
+
+////////////////////////////////// 2a resolução /////////////////////////////////////////
+
+let listaDeMoedas = document.querySelector('ul');
 
 const url = 'https://api.coincap.io/v2/assets';
 
-function fetchCrypto() {
-  fetch(url)
-  .then((resposta) => resposta.json())
-  .then((moedas) => {
-    let arrayDasMoedas = moedas.data;
-    let dezPrimeirasMoedas = arrayDasMoedas.slice(0, 10);
-    
-    dezPrimeirasMoedas.forEach((moeda) =>{
-      let li = document.createElement('li')
-      li.innerHTML = `${moeda.name} ${moeda.symbol} : ${moeda.priceUsd}`
-      listaDeMoedas.appendChild(li)
+const fetchCrypto = async () => {
+  const coins = fetch(url)
+    .then((resposta) => resposta.json())
+    .then((moedas) => {
+      let arrayDasMoedas = moedas.data;
+      let dezPrimeirasMoedas = arrayDasMoedas.slice(0, 10);
+      return dezPrimeirasMoedas;
     })
-  })
-  .catch((erro) => console.log(`Deu ruim! ${erro}`))
+    .catch((erro) => console.log(`Deu ruim! ${erro}`))
+
+  return coins;
 }
 
-fetchCrypto();
+const pegaMoedas = async () => {
+  const dezMoedas = await fetchCrypto();
+  dezMoedas.forEach((moeda) => {
+    let li = document.createElement('li')
+    li.innerHTML = `${moeda.name} ${moeda.symbol} : ${moeda.priceUsd}`
+    listaDeMoedas.appendChild(li)
+  })
+}
+
+pegaMoedas();
